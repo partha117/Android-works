03901e19ea83c5f7a9cbb1c9bf4b31e8b5e65040
partha117 <1305117.pc@ugrad.cse.buet.ac.bd>
2016-05-02 20:06:27 +0600

    Emergency app with database

---
+++ Emergency/app/src/main/java/com/example/partha/emergency/AddressBook.java
@@ -0,0 +1,19 @@
+package com.example.partha.emergency;
+
+/**
+ * Created by Partha on 4/20/2016.
+ */
+public class AddressBook {
+
+    String Organization_name;
+    String Location;
+    String Contact_number;
+
+    public AddressBook(String Organization, String location, String contact_number) {
+        Organization_name = Organization;
+        Location = location;
+        Contact_number = contact_number;
+    }
+
+
+}
